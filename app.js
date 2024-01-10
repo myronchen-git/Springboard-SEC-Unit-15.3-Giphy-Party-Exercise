40a789b7e9cbab81e5eeac9955cec46cff63e2f7
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-10 12:39:50 -0800

    feat: add search functionality
    Added the HTML form, input, and submit button for doing a search.
    
    For JavaScript, added the event listener for clicking on the submit
    button, the handleSubmit method, and the method for contacting the
    Giphy API and doing a search.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Let's get this party started!");
+$(document).ready(() => {
+  new GiphyPartyApp();
+});
+
+class GiphyPartyApp {
+  static giphyApiKey = "zHfDJnPpJz9eeMNhhPmuSjojQ06w3rw7";
+
+  constructor() {
+    $("#form-search").submit(this.handleSubmit.bind(this));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const searchTerm = $("#input-search").val();
+    const imageUrl = this.searchGif(searchTerm);
+    $("#input-search").val("");
+  }
+
+  async searchGif(searchTerm) {
+    try {
+      const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+        params: { api_key: GiphyPartyApp.giphyApiKey, q: searchTerm, limit: 1 },
+      });
+
+      console.log(response);
+      console.log(response.data.data[0].embed_url);
+
+      return response.data.data[0].embed_url;
+    } catch (e) {
+      console.log("Error when contacting giphy:\n", e);
+      return "";
+    }
+  }
+}
